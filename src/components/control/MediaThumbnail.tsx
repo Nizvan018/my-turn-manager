f42476e7c445d0a332297d9892db4e56583c2bca
Nizvan Monteon Ricardez <nizvan.dev@gmail.com>
2025-11-07 18:29:37 -0600

    Feat(MediaController): Add functionality to reorganize the media paths

--- src/components/control/MediaThumbnail.tsx
+++ src/components/control/MediaThumbnail.tsx
@@ -1,3 +1,5 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { Play, Trash } from "lucide-react";
 import { getFileName, getMediaType, toMediaUrl } from "../../lib/mediaHelpers";
 
@@ -17,62 +19,70 @@ interface Props {
  * @returns JSX.Element
  */
 export default function MediaThumbnail({ path, index, isPlaying, goToIndexCallback, removeFileCallback }: Props) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: path });
+    const style = { transform: CSS.Transform.toString(transform), transition }
     const mediaType = getMediaType(path);
 
     return (
-        <div className={`${isPlaying ? "from-curious-blue-500/10" : "from-curious-blue-950/5"} flex gap-4 p-3 rounded-3xl border border-curious-blue-950/10 bg-radial-[at_0%_50%] from-60% to-curious-blue-950/5 transition-colors`}>
-            {/* THUMBNAIL */}
-            <div className="relative min-w-40 max-w-44 h-30 rounded-xl overflow-hidden bg-curious-blue-950/5">
-                {mediaType === "image" ? (
-                    <img
-                        src={toMediaUrl(path)}
-                        alt={getFileName(path)}
-                        className="w-full hfull object-cover"
-                    />
-                ) : (
-                    <>
-                        <video
+        <div
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
+        >
+            <div className={`${isPlaying ? "from-curious-blue-500/10" : "from-curious-blue-950/5"} flex gap-4 p-3 rounded-3xl border border-curious-blue-950/10 bg-radial-[at_0%_50%] from-60% to-curious-blue-950/5 transition-colors`}>
+                {/* THUMBNAIL */}
+                <div className="relative min-w-40 max-w-44 h-30 rounded-xl overflow-hidden bg-curious-blue-950/5">
+                    {mediaType === "image" ? (
+                        <img
                             src={toMediaUrl(path)}
-                            className="w-full h-full object-cover"
+                            alt={getFileName(path)}
+                            className="w-full hfull object-cover"
                         />
-                        <div className="absolute inset-0 flex items-center justify-center bg-curious-blue-950/20">
-                            <Play className="size-6 text-white" />
-                        </div>
-                    </>
-                )}
-            </div>
+                    ) : (
+                        <>
+                            <video
+                                src={toMediaUrl(path)}
+                                className="w-full h-full object-cover"
+                            />
+                            <div className="absolute inset-0 flex items-center justify-center bg-curious-blue-950/20">
+                                <Play className="size-6 text-white" />
+                            </div>
+                        </>
+                    )}
+                </div>
 
-            {/* INFORMATION */}
-            <div className="flex flex-col gap-1">
-                <span className="text-curious-blue-950/60 text-sm">{mediaType === "video" ? "Video" : "Imagen"}</span>
-                <p className="font-medium line-clamp-2">{getFileName(path)}</p>
-                <div className="flex items-center gap-2">
-                    <button
-                        onClick={() => goToIndexCallback(index)}
-                        disabled={isPlaying}
-                        className="flex items-center gap-1 w-fit text-xs py-2 px-3 rounded-full border border-curious-blue-950/10 bg-curious-blue-950/5 duration-200 not-disabled:hover:bg-curious-blue-950/10 not-disabled:cursor-pointer"
-                    >
-                        {isPlaying ? (
-                            <>
-                                <span className="text-curious-blue-600">En reproducción</span>
-                            </>
-                        ) : (
-                            <>
-                                <span>Reproducir</span>
-                                <Play className="size-4" />
-                            </>
-                        )}
-                    </button>
-                    <button
-                        onClick={() => removeFileCallback(index)}
-                        className="flex items-center gap-1 w-fit text-rose-400 text-xs py-2 px-3 border border-transparent duration-200 hover:text-rose-500 cursor-pointer"
-                    >
-                        <span>Quitar</span>
-                        <Trash className="size-4" />
-                    </button>
+                {/* INFORMATION */}
+                <div className="flex flex-col gap-1">
+                    <span className="text-curious-blue-950/60 text-sm">{mediaType === "video" ? "Video" : "Imagen"}</span>
+                    <p className="font-medium line-clamp-2">{getFileName(path)}</p>
+                    <div className="flex items-center gap-2">
+                        <button
+                            onClick={() => goToIndexCallback(index)}
+                            disabled={isPlaying}
+                            className="flex items-center gap-1 w-fit text-xs py-2 px-3 rounded-full border border-curious-blue-950/10 bg-curious-blue-950/5 duration-200 not-disabled:hover:bg-curious-blue-950/10 not-disabled:cursor-pointer"
+                        >
+                            {isPlaying ? (
+                                <>
+                                    <span className="text-curious-blue-600">En reproducción</span>
+                                </>
+                            ) : (
+                                <>
+                                    <span>Reproducir</span>
+                                    <Play className="size-4" />
+                                </>
+                            )}
+                        </button>
+                        <button
+                            onClick={() => removeFileCallback(index)}
+                            className="flex items-center gap-1 w-fit text-rose-400 text-xs py-2 px-3 border border-transparent duration-200 hover:text-rose-500 cursor-pointer"
+                        >
+                            <span>Quitar</span>
+                            <Trash className="size-4" />
+                        </button>
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 }
